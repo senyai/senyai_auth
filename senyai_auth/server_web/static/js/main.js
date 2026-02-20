9a0421b01da20d02afdf196254048560108f040a
Maria Pavlova <pavlovamar96@yandex.ru>
2026-02-20 17:03:45 +0300

    feat: add managing user roles

--- senyai_auth/server_web/static/js/main.js
+++ senyai_auth/server_web/static/js/main.js
@@ -1,4 +1,4 @@
-import { initRoleEditor } from "./components/role_editor.js";
+import { initRoleEditor, getRolePayload } from "./components/role_editor.js";
 import { initToasts } from "./components/toasts.js";
 
 
