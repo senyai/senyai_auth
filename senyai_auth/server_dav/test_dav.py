8f14f9da984e4a1599677cdb020efa6d7d26885b
Arseniy Terekhin <senyai@gmail.com>
2026-04-17 19:00:56 +0300

    test: dav: add permissions test for duplicates

--- senyai_auth/server_dav/test_dav.py
+++ senyai_auth/server_dav/test_dav.py
@@ -13,3 +13,16 @@ class PermissionsTest(TestCase):
         perm = Permissions(["user1"])
         folders = perm.list_children(DAVPath("xx"))
         self.assertIsNone(folders)
+
+    def test_duplicate_permissions(self):
+        perm = Permissions(
+            ["user1", "user1", "user2", "user2:w", "user1:w", "user3", "user3"]
+        )
+        folders = perm.list_children(DAVPath(""))
+        self.assertEqual(folders, ["user1", "user2", "user3"])
+        self.assertTrue(perm.has_read_access(DAVPath("user1")))
+        self.assertTrue(perm.has_write_access(DAVPath("user1")))
+        self.assertTrue(perm.has_read_access(DAVPath("user2")))
+        self.assertTrue(perm.has_write_access(DAVPath("user2")))
+        self.assertTrue(perm.has_read_access(DAVPath("user3")))
+        self.assertFalse(perm.has_write_access(DAVPath("user3")))
