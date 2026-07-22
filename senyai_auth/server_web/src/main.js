e99f1d939e43f4f9f3fe9720d64b38b56689a68d
Arseniy Terekhin <senyai@gmail.com>
2026-07-22 13:07:18 +0300

    feat: web: pack all components into single .js file

--- senyai_auth/server_web/src/main.js
+++ senyai_auth/server_web/src/main.js
@@ -1,6 +1,7 @@
 import { initRoleEditor } from "./components/role_editor.js";
 import { initToasts } from "./components/toasts.js";
 import { initPasswordField } from "./components/password_field.js"
+import "./components/switch_theme.js"
 
 
 initRoleEditor();
